510d9ae43f2be15ee149df07ea2fb53f3cabb005
nemumu <git@nemumu.com>
2013-12-01 23:37:39 +0900

    Update conform url method

--- mailcreate.py
+++ mailcreate.py
@@ -13,9 +13,9 @@ class twimail:
 		req = self.sess.get('http://15qm.com/?act=sevin')
 		html = req.text
 
-		r = re.compile('<input size="30" type="text" value=".{26}"/>')
+		mailRegu = re.compile('<input size="30" type="text" value=".{26}"/>')
 		self.mailAddress = ''
-		match = r.search(html)
+		match = mailRegu.search(html)
 		if match:
 			address = match.group().strip('<input size="30" type="text" value="')
 			address = address.strip('"/>')
@@ -24,17 +24,40 @@ class twimail:
 		else:
 			print "Not Found Authenticity_Token"
 
-	def GetConformMail():
-		req = self.sess.post('http://15qm.com/?act=recv&n=1056')
+	def GetConformMail(self):
+		req = self.sess.post('http://15qm.com/?act=recv')
 		html = req.text
 
-		r = re.compile('<a href="https://twitter.com/account/confirm_email/.{33}" target="_blank">')
-		match = r.search(html)
+		# 受信確認を行い、Twitterの確認メールへのリンクだけを取り出す
+		checkRegu = re.compile('http://15qm.com/\?act=mde&amp;mid=%3C.{20}%40spruce-goose.twitter.com%3E&amp;no=1&n=\d{4}')
+		mailUrl = ""
+		match = checkRegu.search(html)
+		if match:
+			mailUrl = match.group()
+			mailUrl = mailUrl.replace('&amp;', '&')
+			mailUrl = mailUrl.replace('%3C', '<')
+			mailUrl = mailUrl.replace('%40', '@')
+			mailUrl = mailUrl.replace('%3E', '>')
+
+			print "MailUrl:%s" % mailUrl
+		else:
+			print "Did not receive conform mail"
+			print html
+			return ""
+		
+		# 読み取ったURLから 
+		req = self.sess.post(mailUrl)
+		html = req.text
+		# 取り出した確認メールから確認用URLだけを取り出す
+		conformRegu = re.compile('https://twitter.com/account/confirm_email/[\w]+/[\w-]{15,20}')
+		conformUrl = ""
+		match = conformRegu.search(html)
                 if match:
-                        conformUrl = match.group().strip('<a href="https://twitter.com/account/confirm_email/')
-                        conformUrl = conformUrl.strip('" target="_blank">')	
+			conformUrl = match.group()
                         print "ConformUrl:%s" % conformUrl
 			return conformUrl
                 else:
                         print "Not Found ConformUrl"
+			print html
+			return ""
 
