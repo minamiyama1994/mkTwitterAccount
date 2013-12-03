304ef3197f67dc1f0f8c28eea656413c67aab5aa
nemumu <git@nemumu.com>
2013-12-04 00:18:36 +0900

    Add conform method

--- twicreate.py
+++ twicreate.py
@@ -1,6 +1,5 @@
 # coding:utf-8
 import urllib2
-import config # Need this file!
 import requests # Need to install!
 import re
 
@@ -40,3 +39,12 @@ class mktwi:
 		params ={'authenticity_token' : authKey, 'user[name]' : screen_name, 'user[email]' : mailAddress, 'user[user_password]' : password, 'user[screen_name]' : user_id, 'user[remember_me_on_signup]' : '0', 'user[use_cookie_personalization]' : '0', 'asked_cookie_personalization_setting' : '1', 'context' : '', 'ad_id' : '', 'ad_ref' : '', 'submit_button' : 'アカウントを作成する', 'user[discoverable_by_email]' : '1', 'user[send_email_newsletter]' : '1'}
 		self.cookie = {'cookie_Session' : cookie_Session, 'cookie_GuestID' : cookie_GuestID}
 		r = self.sess.post(url,cookies=cookie, params=params)
+		print r.cookies
+	
+	def conform(self, conformURL):
+		############################
+		# Conform MailAddress
+		############################
+		r = self.sess.post(conformURL, cookies=self.cookie)
+		print r.text
+		return	
