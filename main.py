7a669eaf440517b66353e272d6ac91a6cc5b8f01
nemumu <git@nemumu.com>
2013-12-05 02:35:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 # coding:utf-8
-import sys
-import requests # Need to install!
 import mailcreate
 import twicreate
 import random
 import time
+
+##############################
+# Define config
+##############################
+authURL = "http://example.com/"
+authReturnURL = "http://example.com/return.cgi"
+
+#############################
+# Example to use method
+#############################
 
 conformMail = mailcreate.twimail()
 mailAddress = conformMail.mailAddress
@@ -17,10 +25,11 @@ print "password:%s" % password
 mkTwitter = twicreate.mktwi(user_id, password, mailAddress, user_id)
 
 print "Wait to receive mail"
-for i in range (1,20):
+for i in range (1,30):
 	print i
 	time.sleep(1)
 conformURL = conformMail.GetConformMail()
-print conformURL
+print "ConformURL:%s" % conformURL
 mkTwitter.conform(conformURL)
-print "Fin"
+mkTwitter.auth(authURL, authReturnURL)
+print "Finish"
